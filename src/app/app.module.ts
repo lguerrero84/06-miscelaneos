5f87f116af1c64ecc71123bf0426e37844391d7d
proveedor_laguerrero <proveedor_laguerrero@ath.com.co>
2019-10-22 17:20:34 -0500

    clase 145 completa dia 22 de octubre de 2019

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {APP_ROUTING} from './app.routes';
 import { AppComponent } from './app.component';
 import { NgStyleComponent } from './components/ng-style/ng-style.component';
 import { CssComponent } from './components/css/css.component';
 import { ClasesComponent } from './components/clases/clases.component';
 import { ResaltadoDirective } from './directives/resaltado.directive';
+import { NgSwitchComponent } from './components/ng-switch/ng-switch.component';
+import { HomeComponent } from './components/home/home.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import { UsuarioEditarComponent } from './components/usuario/usuario-editar.component';
+import { UsuarioDetalleComponent } from './components/usuario/usuario-detalle.component';
+import {UsuarioNuevoComponent} from './components/usuario/usuario-nuevo.component'
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -13,10 +20,18 @@ import { ResaltadoDirective } from './directives/resaltado.directive';
     NgStyleComponent,
     CssComponent,
     ClasesComponent,
-    ResaltadoDirective
+    ResaltadoDirective,
+    NgSwitchComponent,
+    HomeComponent,
+    UsuarioComponent,
+    UsuarioEditarComponent,
+    UsuarioDetalleComponent,
+    UsuarioNuevoComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
